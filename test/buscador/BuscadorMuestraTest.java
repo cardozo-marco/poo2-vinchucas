091b2757a4fcc153810ba7cd90ad904595bd5c3e
Franco Paredes <francoismaelparedes@hotmail.com>
2025-06-13 02:29:30 -0300

    test buscador muestra

--- test/buscador/BuscadorMuestraTest.java
+++ test/buscador/BuscadorMuestraTest.java
@@ -18,6 +18,50 @@ import usuario.Usuario;
 
 class BuscadorMuestraTest {
 
-	
+	private BuscadorMuestra buscador;
+    private Muestra muestra1;
+    private Muestra muestra2;
+
+    @BeforeEach
+    public void setUp() {
+        buscador = new BuscadorMuestra();
+
+        muestra1 = new Muestra("foto1", new Ubicacion(0.0, 0.0), new Usuario("user1", "frank"), Especie.INFESTANS, LocalDate.of(2023, 6, 1), new EstadoAbierta());
+        muestra2 = new Muestra("foto2", new Ubicacion(1.1, 1.1), new Usuario("user2", "franks"), Especie.GUASAYANA, LocalDate.of(2023, 6, 10), new EstadoAbierta());
+    }
+
+    @Test
+    public void testBuscarConFiltroEspecie() {
+        Filtro filtroEspecie = new FiltroTipoInsectoMuestra(TipoOpinion.VINCHUCA);
+
+        List<Muestra> resultado = buscador.buscar(Arrays.asList(muestra1, muestra2), filtroEspecie);
+
+        assertEquals(1, resultado.size());
+        assertTrue(resultado.contains(muestra1));
+    }
+
+    @Test
+    public void testBuscarConFiltroCompuestoAND() {
+        muestra1.verificar();
+
+        Filtro filtroEspecie = new FiltroTipoInsectoMuestra(TipoOpinion.VINCHUCA);
+        Filtro filtroVerificada = new FiltroMuestraVerificada();
+
+        Filtro filtroCompuesto = new FiltroAnd(filtroEspecie, filtroVerificada);
+
+        List<Muestra> resultado = buscador.buscar(Arrays.asList(muestra1, muestra2), filtroCompuesto);
+
+        assertEquals(1, resultado.size());
+        assertTrue(resultado.contains(muestra1));
+    }
+
+    @Test
+    public void testBuscarSinCoincidencias() {
+        Filtro filtroFecha = new FiltroFechaCreacionMuestra(LocalDate.of(2025, 1, 1)); //ninguna muestra tiene esa fecha
+
+        List<Muestra> resultado = buscador.buscar(Arrays.asList(muestra1, muestra2), filtroFecha);
+
+        assertTrue(resultado.isEmpty());
+    }
 
 }
