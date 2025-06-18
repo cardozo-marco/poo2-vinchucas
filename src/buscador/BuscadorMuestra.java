52faf65ca6ce3dde628334b36deb04fb9081a471
Franco Paredes <francoismaelparedes@hotmail.com>
2025-06-18 01:35:03 -0300

    se agrego comentarios al buscador

--- src/buscador/BuscadorMuestra.java
+++ src/buscador/BuscadorMuestra.java
@@ -5,7 +5,10 @@ import java.util.stream.Collectors;
 
 import muestra.Muestra;
 
-public class BuscadorMuestra { //resuelto con Composite. Filtros compuestos como FiltroAnd y FiltroOr y el resto de filtros simples son leaf.
+public class BuscadorMuestra { 
+	//resuelto con Composite. Filtros compuestos como FiltroAnd y FiltroOr y el resto de filtros simples son leaf.
+	//cumple con el principio de single responsability ya que se encarga solamente de aplicar un filtro a una coleccion de muestras.
+	//component: Filtro client: BuscadorMuestra composite: FiltroAnd, FiltroOr leaf: resto de filters simples
 	public List<Muestra> buscar(List<Muestra> muestras, Filtro filtro) {
         return muestras.stream()
             .filter(m -> filtro.aplica(m))
